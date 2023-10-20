0d44189c117d0de89a71a7e2d9f049ff520cdc3a
keisei4064 <21t4064b@shinshu-u.ac.jp>
2023-10-20 16:39:24 +0900

    データ取得機能追加

--- real_kachaka.py
+++ real_kachaka.py
@@ -4,6 +4,7 @@ import numpy as np
 from IPython.display import Image, display
 import asyncio
 import kachaka_api
+import pickle
 
 
 class RealKachaka(KachakaBase):
@@ -12,10 +13,17 @@ class RealKachaka(KachakaBase):
         self.client = kachaka_api.KachakaApiClient()  # 同期ライブラリ
         self.client.update_resolver()
         self.client.set_auto_homing_enabled(False)
+        self.update_sensor_count = 0
         self.update_sensor_data()
 
     def move_to_pose(self, distination: Pose):
-        self.client.move_to_pose(distination.x, distination.y, distination.theta)
+        ratio = 1000
+        self.client.move_to_pose(
+            distination.x / 1000, distination.y / 1000, distination.theta
+        )
+
+        while client.is_command_running():
+            pass
 
     def is_moving_finished(self) -> bool:
         return not self.client.is_command_running()
@@ -40,3 +48,22 @@ class RealKachaka(KachakaBase):
         ax = plt.subplot()
         ax.set_aspect("equal")
         ax.scatter(dist * np.cos(theta), dist * np.sin(theta))
+
+        # ファイルをバイナリモードで開く
+        # オブジェクトをシリアライズしてファイルに書き込む
+        with open(
+            "sensor_log/theta{}.pkl".format(self.update_sensor_count), "wb"
+        ) as file:
+            pickle.dump(theta, file)
+
+        with open(
+            "sensor_log/dist{}.pkl".format(self.update_sensor_count), "wb"
+        ) as file:
+            pickle.dump(dist, file)
+
+        with open(
+            "sensor_log/position{}.pkl".format(self.update_sensor_count), "wb"
+        ) as file:
+            pickle.dump(position, file)
+
+        self.update_sensor_count += 1
